ec940303e318175c934659067986d401dd3f4a78
youan <18768869296@163.com>
2020-11-09 14:49:39 +0800

    Initial Commit

---
+++ bilitest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  bilitest
+//
+//  Created by Bilibili on 2020/11/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ bilitest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  bilitest
+//
+//  Created by Bilibili on 2020/11/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
